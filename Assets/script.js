f9bde88542ad0b571f96b54f24acb4732b93aebd
justin moran <justinmoranre@gmail.com>
2022-12-05 22:22:07 -0500

    finished timer starting questions loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,40 +1,35 @@
-var countdown = 60;
-
-var interval = setInterval(function()
-{
-    document.getElementById("timer").innerHTML = "Time left: " + -- countdown;
-
-    if (countdown == 0)
-        clearInterval(interval);
-
-}, 1000);
 
 
+var timerEl = document.getElementById('timer');
+var directionsEl = document.getElementById("directions");
 
-// function countdown(minutes) {
-//     var seconds = 60;
-//     var mins = minutes
-//     function tick() {
-//         //This script expects an element with an ID = "counter". You can change that to what ever you want. 
-//         var counter = document.getElementById("timer");
-//         var current_minutes = mins-1
-//         seconds--;
-//         timer.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-//         if( seconds > 0 ) {
-//             setTimeout(tick, 1000);
-//         } else {
-//             setTimeout(function(){
-//                 if(mins > 1){
-//                 countdown(mins-1);
-//                 }
-//                 },1000);           
-//             } 
-//         }
-    
-//     tick();
-// };
+function displayTime(seconds) {
+  var label = "seconds";
+  if (seconds === 1) {
+    label = "second";
+  }
+  timerEl.textContent = seconds + " " + label;
+}
 
-var questions = [
+function countdown() {
+  var timeLeft = 5;
+  displayTime(timeLeft);
+  
+  var timeInterval = setInterval(function () {
+    timeLeft--;
+    displayTime(timeLeft);
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+      startQuiz();
+      timerEl.textContent = "Expired";
+
+    }
+  }, 1000);
+};
+
+
+function startquiz() {
+  let questions = [
     {
       headline: "Question 1",
       answers: {
@@ -76,4 +71,9 @@ var questions = [
     correct: "b",
     },   
     
-  ];
+  ]; 
+  for(i=0; i < questions.length; i++) {
+    directions.
+  };
+
+};
